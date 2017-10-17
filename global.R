ddeacdd19c735f7a6bbccc5572c974cdc493340a
bnasr <bijan.s.nasr@gmail.com>
2017-10-17 14:39:41 -0700

    midday list url and siteinfo url

--- global.R
+++ global.R
@@ -3,10 +3,14 @@ TEST_MODE <- TRUE
 if(getwd()=='/home/bijan/Projects/drawROI') TEST_MODE <- TRUE
 if(getwd()=='/home/shiny/apps/drawROI') TEST_MODE <- TRUE
 
+middayimglistURL <- 'https://canopy.sr.unh.edu/webcam/network/middayimglist/'
+sitesInfoURL <- 'https://phenocam.sr.unh.edu/webcam/network/siteinfo/'
+
 mountPath <- ''
 
 if(TEST_MODE) mountPath <- '/mnt/klima'
 
 midddayListPath <- paste0(mountPath, '/home/shiny/middayList/')
+# midddayListPath <- NULL
 clImagePath <- paste0(mountPath, '/home/shiny/climages/')
 
